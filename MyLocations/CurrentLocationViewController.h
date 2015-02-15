9405c8c5f0a241f8f8aad5955c1d5a81e21efcf8
Ajay Singh <ajayxsingh@gmail.com>
2015-02-15 16:53:10 -0500

    GPS Coordinates

---
+++ MyLocations/CurrentLocationViewController.h
@@ -0,0 +1,32 @@
+//
+//  FirstViewController.h
+//  MyLocations
+//
+//  Created by Ajay Singh on 2/14/15.
+//  Copyright (c) 2015 Ajay Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreLocation/CoreLocation.h>
+
+
+@interface CurrentLocationViewController : UIViewController <CLLocationManagerDelegate>
+
+@property (nonatomic, weak) IBOutlet UILabel *messageLabel;
+
+@property (nonatomic, weak) IBOutlet UILabel *latitudeLabel;
+
+@property (nonatomic, weak) IBOutlet UILabel *longitudeLabel;
+
+@property (nonatomic, weak) IBOutlet UILabel *addressLabel;
+
+@property (nonatomic, weak) IBOutlet UIButton *tagButton;
+
+@property (nonatomic, weak) IBOutlet UIButton *getButton;
+
+-(IBAction)getLocation:(id)sender;
+
+
+@end
+
